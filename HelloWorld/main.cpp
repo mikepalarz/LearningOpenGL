0f0305db6f8b992079aa88cc81ada06615b564ce
Mike Palarz <mpalarz1@gmail.com>
2019-03-19 10:41:25 -0500

    We've moved beyond shapes, hooray! Now I'm able to render a texture, which is essentially overlaying an image onto a shape. In this particular case, I'm showing a simple box image within the window.

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -7,6 +7,7 @@
 //
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "stb_image.h"
 #include "myShader.h"
 #include <iostream>
 #include <cmath>
@@ -66,82 +67,69 @@ int main(int argc, const char * argv[]) {
     /*
      Building and compiling our shaders
      */
-    
-    // Vertex shader
-//    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-//    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-//    glCompileShader(vertexShader);
-//    // Checking for vertex shader compile errors
-//    int success;
-//    char infoLog[512];
-//    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-//    if (!success) {
-//        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-//        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-//    } else {
-//        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-//        std::cout << "SUCCESS::SHADER::VERTEX::COMPILATION_COMPLETED\n" << infoLog << std::endl;
-//    }
-//
-//    // Fragment shader for the color orange
-//    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-//    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-//    glCompileShader(fragmentShader);
-//    // Again, checking for compile-time errors for the fragment shader
-//    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-//    if (!success) {
-//        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-//        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-//    } else {
-//        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-//        std::cout << "SUCCESS::SHADER::FRAGMENT::COMPILATION_COMPLETED\n" << infoLog << std::endl;
-//    }
-//
-//    // We now link the shaders for our first program
-//    int shaderProgram = glCreateProgram();
-//    glAttachShader(shaderProgram, vertexShader);
-//    glAttachShader(shaderProgram, fragmentShader);
-//    glLinkProgram(shaderProgram);
-//    // Checking for linking errors
-//    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-//    if (!success) {
-//        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-//        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-//    } else {
-//        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-//        std::cout << "SUCCESS::SHADER::PROGRAM::LINKING_COMPLETED\n" << infoLog << std::endl;
-//    }
-//
-//    glDeleteShader(vertexShader);
-//    glDeleteShader(fragmentShader);
-    
     Shader myShader("shader.vs", "shader.fs");
     
     /*
      Setting up vertex data, VBOs, and VAOs
      */
-    float vertices [] = {
-        // Positions        // Colors
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // Lower left, red
-        0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // Middle top, green
-        0.0f,   0.5f, 0.0f, 0.0f, 0.0f, 1.0f   // Lower right, blue
+    float vertices[] = {
+         // Positions         // Colors           // Texture coordinates
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top right corner
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom right corner
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom left corner
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top left corner
     };
     
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {
+        0, 1, 3,    // First triangle
+        1, 2, 3     // Second triangle
+    };
+    
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     
     // Binding the VAO, then binding and filling the VBO and enabling our attribute(s)
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * (sizeof(float)), (void*)0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * (sizeof(float)), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * (sizeof(float)), (void*) (3 * sizeof(float)));
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * (sizeof(float)), (void*) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    // Texture coordinate attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * (sizeof(float)), (void*) (6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     
     // This can be used to draw our objects in wireframe mode
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
+    // Generating our texture
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // Setting the texture wrapping and filtering options for the currently bound texture object
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Load and generate the texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
     
     // This is our render loop
     while (!glfwWindowShouldClose(window)) {
@@ -152,11 +140,14 @@ int main(int argc, const char * argv[]) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);   // Configuring the color buffer for when the screen will be cleared
         glClear(GL_COLOR_BUFFER_BIT);
         
-        // Drawing our first triangle
+        // Binding the texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+        // Render the container
         myShader.use();
-        
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);    // Actually drawing the triangles
+        // glDrawArrays(GL_TRIANGLES, 0, 3);    // Actually drawing the triangles
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
         
         // Checks and calls I/O events; swap buffers
         glfwSwapBuffers(window);
@@ -166,6 +157,7 @@ int main(int argc, const char * argv[]) {
     // De-allocating all resources once they've outlived their purpose
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     
     glfwTerminate();
     return 0;
