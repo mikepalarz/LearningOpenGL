7e327744e5f6a92434482e12b627304bcef632f3
Mike Palarz <mpalarz1@gmail.com>
2019-03-22 13:42:26 -0500

    Finally in the 3D world! This time, I created a window which contains multiple 3D cubes that are rotating and are spread throughout the window.

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -62,12 +62,70 @@ int main(int argc, const char * argv[]) {
      Setting up vertex data, VBOs, and VAOs
      */
     float vertices[] = {
-         // Positions         // Colors           // Texture coordinates
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top right corner
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom right corner
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom left corner
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top left corner
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
+    
+    // Defining the position for 10 cubes
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+    
+//    float vertices[] = {
+//         // Positions        // Texture coordinates
+//         0.5f,  0.5f, 0.0f,  1.0f, 1.0f, // Top right corner
+//         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, // Bottom right corner
+//        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, // Bottom left corner
+//        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f  // Top left corner
+//    };
     
     unsigned int indices[] = {
         0, 1, 3,    // First triangle
@@ -87,14 +145,11 @@ int main(int argc, const char * argv[]) {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * (sizeof(float)), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * (sizeof(float)), (void*)0);
     glEnableVertexAttribArray(0);
-    // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * (sizeof(float)), (void*) (3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
     // Texture coordinate attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * (sizeof(float)), (void*) (6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * (sizeof(float)), (void*) (3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     
     // This can be used to draw our objects in wireframe mode
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -145,6 +200,9 @@ int main(int argc, const char * argv[]) {
     myShader.setInt("texture1", 0);
     myShader.setInt("texture2", 1);
     
+    // Enabling depth testing
+    glEnable(GL_DEPTH_TEST);
+    
     // This is our render loop
     while (!glfwWindowShouldClose(window)) {
         // Handling input
@@ -152,7 +210,7 @@ int main(int argc, const char * argv[]) {
         
         // Actual rendering commands
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);   // Configuring the color buffer for when the screen will be cleared
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         // Binding the texture
         glActiveTexture(GL_TEXTURE0);   // Making sure to activate the first texture unit
@@ -162,19 +220,33 @@ int main(int argc, const char * argv[]) {
         
         myShader.setFloat("mixSetting", mixValue);  // Set the texture mix value within the fragment shader
         
-        // Creating transformations
-        glm::mat4 transform = glm::mat4(1.0f);  // Initializing the transform matrix into an identity matrix first
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-        transform = glm::rotate(transform, (float) glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        
         // Render the container
         myShader.use();
-        unsigned int transformLocation = glGetUniformLocation(myShader.ID, "transform");
-        glUniformMatrix4fv(transformLocation, 1, GL_FALSE, glm::value_ptr(transform));
+        
+        // Setting up our model, view, and projection matrices
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 projection = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float) (SCREEN_WIDTH / SCREEN_HEIGHT), 0.1f, 100.0f);
+        
+        myShader.setMat4("view", view);
+        myShader.setMat4("projection", projection);
         
         glBindVertexArray(VAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);    // Actually drawing the triangles
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        for (unsigned int i = 0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            if (i % 3 == 0) {
+                angle = glfwGetTime() * 25.0f;
+            }
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            myShader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);    // Actually drawing the triangles
+        }
+        
+        // glDrawArrays(GL_TRIANGLES, 0, 36);    // Actually drawing the triangles
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
         // Checks and calls I/O events; swap buffers
         glfwSwapBuffers(window);
